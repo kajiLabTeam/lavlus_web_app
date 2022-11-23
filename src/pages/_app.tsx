cf9acd5bfa5ab4d1f0d8c17a5833e18cf0c7d970
refty10 <panzou.xx7@gmail.com>
2022-11-23 18:49:07 +0900

    refactor: app & index pageを修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,10 @@
-import type { AppProps } from "next/app";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import { DashboardLayout, StandardLayout } from "../components/layouts";
-import { Auth } from "../components";
 import { RecoilRoot } from "recoil";
 import { SWRConfig } from "swr";
-import { fetcher } from "../utils";
 import "yakuhanjp/dist/css/yakuhanjp_s.css";
+import { Auth } from "@/components";
+import { fetcher } from "@/utils";
+import { AppPropsWithLayout } from "@/types";
 
 const theme = extendTheme({
   fonts: {
@@ -14,24 +13,15 @@ const theme = extendTheme({
   },
 });
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
   return (
     <ChakraProvider theme={theme}>
-      {/* @ts-ignore */}
       <RecoilRoot>
         <SWRConfig value={{ fetcher, refreshInterval: 3000 }}>
           <Auth authenticated={!!pageProps.authenticated}>
-            {pageProps.layout === "dashboard" ? (
-              <DashboardLayout>
-                <Component {...pageProps} />
-              </DashboardLayout>
-            ) : pageProps.layout === "standard" ? (
-              <StandardLayout>
-                <Component {...pageProps} />
-              </StandardLayout>
-            ) : (
-              <Component {...pageProps} />
-            )}
+            {getLayout(<Component {...pageProps} />)}
           </Auth>
         </SWRConfig>
       </RecoilRoot>
