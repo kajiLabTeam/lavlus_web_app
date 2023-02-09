13f3b1ea5a42b5b3adac86a9edd91dd541b5a157
Refty <panzou.xx7@gmail.com>
2023-02-09 10:51:37 +0900

    fix: 不必要な要素を消した

--- src/pages/[username]/index.tsx
+++ src/pages/[username]/index.tsx
@@ -1,29 +1,10 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import {
-  Box,
-  Text,
-  Image,
-  Button,
-  Avatar,
-  Stack,
-  Grid,
-  GridItem,
-  Center,
-  Input,
-  Container,
-  Tag,
-  HStack,
-  Link,
-  Card,
-} from '@chakra-ui/react';
+import { Stack, Grid, GridItem, Center, Container } from '@chakra-ui/react';
 import { ProjectCard, ProjectListMenu, RequesterInfoView } from '@/components/pages/projectList';
-import { LavlusIcon } from '@/components/icons';
 // SWR
 import { User, Project } from '@/types';
 import useSWR from 'swr';
-// Utils
-import { format } from 'date-fns';
 // NextPage
 import { NextPageWithLayoutAndPageExtraInfo } from '@/types';
 import { StandardLayout } from '@/layouts';
@@ -35,26 +16,23 @@ const ProjectList: NextPageWithLayoutAndPageExtraInfo = () => {
   const { data: user } = useSWR<User>([`/users/requester/name/${username}`]);
 
   return (
-    <>
-      <Container maxW="container.xl" pt={16}>
-        <Grid templateColumns="360px 1fr" gap={8} px={4}>
-          <GridItem>
-            <Center>{user && <RequesterInfoView user={user} />}</Center>
-          </GridItem>
-
-          <GridItem>
-            <ProjectListMenu />
-            <Stack mt={8} spacing={8}>
-              {projects &&
-                user &&
-                projects.map((project) => (
-                  <ProjectCard project={project} user={user} key={project.id} />
-                ))}
-            </Stack>
-          </GridItem>
-        </Grid>
-      </Container>
-    </>
+    <Container minH="inherit" maxW="container.xl" pt={16}>
+      <Grid templateColumns="360px 1fr" gap={8} px={4}>
+        <GridItem>
+          <Center>{user && <RequesterInfoView user={user} />}</Center>
+        </GridItem>
+        <GridItem>
+          <ProjectListMenu />
+          <Stack mt={8} spacing={8}>
+            {projects &&
+              user &&
+              projects.map((project) => (
+                <ProjectCard project={project} owner={user} key={project.id} />
+              ))}
+          </Stack>
+        </GridItem>
+      </Grid>
+    </Container>
   );
 };
 
